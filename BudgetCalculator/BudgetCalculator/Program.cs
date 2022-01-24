5cd04c1fd9015520e25630615508c1cea7761c7b
ThatNicDude <nic.alves48@gmail.com>
2022-01-24 00:11:28 +0000

    Finished the first iteration of the questionare

--- BudgetCalculator/BudgetCalculator/Program.cs
+++ BudgetCalculator/BudgetCalculator/Program.cs
@@ -12,19 +12,19 @@ namespace BudgetCalculator
         //because I dont have much money
 
         //storing Variables
-        float IncomeAfterTax;
-        float HousingCost; 
-        float UtiliyCost;  
-        float CarFinance;  
-        float CarInsurance; 
-        float MotorCycleFinance; 
-        float MotorcycleInsurance; 
-        float CreditCostPM;
-        float AverageFuelPM;
-        float Savings;
-        float PlannedSpending;
+        static float IncomeAfterTax;
+        static float HousingCost;
+        static float UtiliyCost;
+        static float CarFinance;
+        static float CarInsurance;
+        static float MotorCycleFinance;
+        static float MotorcycleInsurance;
+        static float CreditCostPM;
+        static float AverageFuelPM;
+        static float Savings;
+        static float PlannedSpending;
 
-        void Questionair()
+        static void Questionair()
         {
             Console.WriteLine("Welcome to your handy monthly budget calculator\n" +
                 "Lets start with a couple questions about numbers to get an idea of your monthly budget.\n" +
@@ -42,20 +42,73 @@ namespace BudgetCalculator
             Console.WriteLine("What is your total utility bill cost P/M? Water, Electrics, Gass..  ect?  ");
             UtiliyCost = float.Parse(Console.ReadLine());
 
-            //Gather monthly cost for Car finance
+            //Gather the monthly cost for Car finance
             Console.WriteLine("What is the payment on your car?  ");
             CarFinance = float.Parse(Console.ReadLine());
 
+            //Gather the monthly cost for car insurance
+            Console.WriteLine("What is your monthly payment on car insurance?  ");
+            CarInsurance = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Do you have a motorcycle? yes or no?");
+            string OwnMotorcycle = Console.ReadLine();
+
+            if (OwnMotorcycle != null || OwnMotorcycle == "yes")
+            {
+                //Gather the monthly cost of MotorCycle finance payments
+                Console.WriteLine("How much do you pay for motorcycle finance?  ");
+                MotorCycleFinance = float.Parse(Console.ReadLine());
+
+                //Gather the monthly cost of MotorCycle Insurance payments               
+                Console.WriteLine("How much do you pay for motorcycle finance?  ");
+                MotorcycleInsurance = float.Parse(Console.ReadLine());
+
+            }
+            else
+            {
+                return;
+            }
+
+            //Gather the monthly cost of credit payments
+            Console.WriteLine("What is your monthly cost of credit payments  ");
+            CreditCostPM = float.Parse(Console.ReadLine());
+
+            //Gather the average fuel spending P/M
+            Console.WriteLine("What is your monthly average spent on fuel?  ");
+            AverageFuelPM = float.Parse(Console.ReadLine());
+
+            //Gather planned spending
+            Console.WriteLine("Do you have any planned spending this month? like a gift or something?  ");
+            PlannedSpending = float.Parse(Console.ReadLine());
 
 
-            Console.ReadKey();
         }
 
-        void Main(string[] args)
+        static void TestValues()
         {
-            Questionair(); 
+            //testing to make sure the values display correctly
+            Console.WriteLine("£" + IncomeAfterTax);
+            Console.WriteLine("£" + HousingCost);
+            Console.WriteLine("£" + UtiliyCost);
+            Console.WriteLine("£" + CarFinance);
+            Console.WriteLine("£" + CarInsurance);
+            Console.WriteLine("£" + MotorCycleFinance);
+            Console.WriteLine("£" + MotorcycleInsurance);
+            Console.WriteLine("£" + CreditCostPM);
+            Console.WriteLine("£" + AverageFuelPM);
+            Console.WriteLine("£" + Savings);
+            Console.WriteLine("£" + PlannedSpending);
+        }
 
+        static void Main(string[] args)
+        {
+            Console.Title = "MontlyBudgetCalculator";
 
+            Questionair();
+
+            TestValues();  
+
+            Console.ReadKey();
         }        
     }
 }
