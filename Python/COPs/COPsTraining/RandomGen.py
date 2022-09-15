53eb41584d8fa787cec2f14f9833948cc7964cd7
ThatNicDude <nic.alves48@gmail.com>
2022-09-15 14:36:12 +0100

    Random Practice

---
+++ Python/COPs/COPsTraining/RandomGen.py
@@ -0,0 +1,11 @@
+import random
+
+print('Hello there, you are here because you cannot decide what cars to look at right now.'
+      'Here is what we think you should look at right now:'
+      '')
+
+# create list of cars:
+cars = ['Toyata', 'Merc', 'VW', 'Audi', 'Vauxhal']
+
+print(random.choice(cars))
+
