3a07350625636eb9e4372a346dd785d8df88aeea
ThatNicDude <nic.alves48@gmail.com>
2022-11-04 20:40:53 +0000

    moving stuff around

--- Python/COPs/COPsTraining/Rando Practice.py
+++
@@ -1,9 +0,0 @@
-print('Welcome to the rollercoaster!')
-height = int(input('What is your height in cm? '))
-
-if height > 120:
-    print('You can ride the rollercoaster!')
-else:
-    print('Sorry you are not tall enough to ride this rollercoaster.')
-
-
