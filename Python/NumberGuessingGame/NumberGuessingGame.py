126ee469482e3225f2a8be70967a9b39f34acb8c
ThatNicDude <nic.alves48@gmail.com>
2022-11-04 21:42:56 +0000

    Finished base game
    The game works as intended. Will most likely add some more detail in the future.

--- Python/NumberGuessingGame/NumberGuessingGame.py
+++ Python/NumberGuessingGame/NumberGuessingGame.py
@@ -8,6 +8,12 @@ os.system('cls')
 GeneratedNumber = random.randint(1, 100)
 Attempts = 0
 
+print("===================================")
+print("")
+print("======= NumberGuessingGame ========")
+print("")
+print("===================================")
+
 print(f"Hey there! "
       f"To play this game, you will need to guess the number that the computer has chosen between from 1 to 100")
 
@@ -19,15 +25,22 @@ time.sleep(2)
 
 UserGuess = int(input("Please choose a number between 1 and 100: "))
 
+NumberList = {1, 100}
+
 while UserGuess != GeneratedNumber:
       Attempts += 1
-      UserGuess = int(input("Please choose a number between 1 and 100: "))
+      
       if UserGuess > GeneratedNumber:
+
             print("You number is higher than the Computer generate number.")
       elif UserGuess < GeneratedNumber:
             print("You number is lower than the Computer generate number.")
       
+      UserGuess = int(input("Please choose a number between 1 and 100: "))
+
+      
 
 
 time.sleep(1)
+os.system('cls')
 print(f"Nice one! You did it in {Attempts} attempts!")
