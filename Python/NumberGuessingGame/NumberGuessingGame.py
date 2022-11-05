d99980b59fc0c833106e955f979330502812f47b
ThatNicDude <nic.alves48@gmail.com>
2022-11-05 21:05:52 +0000

    Fleshing out the test website

--- Python/NumberGuessingGame/NumberGuessingGame.py
+++ Python/NumberGuessingGame/NumberGuessingGame.py
@@ -1,19 +1,24 @@
 # This is a game that will generate a game and the user will need to guess what that number is.
 
+#importing the modules I need so far.
 import random
 import time
 import os
 
+# Clearing the console/terminal so there is no clutter.
 os.system('cls')
+# generating a random number that the user will then need to guess.
 GeneratedNumber = random.randint(1, 100)
+# Setting the number of attmpts veriable to 0 is just to create it into existance for us to manipulate downt he line.
 Attempts = 0
 
 print("===================================")
-print("")
+print("***********************************")
 print("======= NumberGuessingGame ========")
-print("")
+print("***********************************")
 print("===================================")
 
+# Creating a delay in the program to give it more "life"
 time.sleep(3)
 
 print(f"Hey there! "
@@ -25,10 +30,12 @@ print(f"The computer will now chose a number from 1 to 100.....")
 
 time.sleep(1)
 
+# taking the users input of a number, the same will be repeated in the while loop if they dont get it right on the first go.
 UserGuess = int(input("Please choose a number between 1 and 100: "))
 
 NumberList = {1, 100}
 
+# This will make sure the program loops if the user does not enter the correct answer
 while UserGuess != GeneratedNumber:
       Attempts += 1
       
@@ -40,9 +47,9 @@ while UserGuess != GeneratedNumber:
       
       UserGuess = int(input("Please choose a number between 1 and 100: "))
 
-      
-
 
 time.sleep(1)
 os.system('cls')
-print(f"Nice one! You did it in {Attempts} attempts!")
+print(f"Nice one! You did it in {Attempts} attempts!"
+      "Try again to beat your score!")
+
