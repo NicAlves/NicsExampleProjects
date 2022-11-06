5698820721bfafe7fd3ca03771309993f2396c0d
ThatNicDude <nic.alves48@gmail.com>
2022-11-06 03:14:10 +0000

    Creating a monthly budget calculator in python!

---
+++ Python/NumberGuessingGame/BudgetCalculator.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+os.system("cls")
+
+print("Welcome!")
+time.sleep(1)
+print("initialising.....")
+time.sleep(1)
+print("This is a tool that you can use to calculate your monthly budget!")
+print("")
+
+MonthlySalary = float(input("What is your monthly income after tax?: "))
+
+
--- Python/NumberGuessingGame/Test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system("cls")
-Attempts = 0
-
-print("===================================")
-print("======= !!!!!NICE ONE!!!!! ========")
-print(f"     You did it in {Attempts} Attempts!!")
-print("") 
